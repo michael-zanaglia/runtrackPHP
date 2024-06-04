201fa3031662c92ba09c692a84db1d3444877310
michael-zanaglia <michael.zanaglia@laplateforme.io>
2024-06-04 11:48:16 +0200

    table key/value

--- Jour02/Job02/index.php
+++ Jour02/Job02/index.php
@@ -1,9 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Job03</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+        }
+        .entete {
+            background-color: #b4b0b0;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding : 5px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
 <?php
-    $count = 0;
-    foreach($_GET as $arg){
-        echo "$arg<br>";
-        $count += 1;
-    }
-
-    echo "Il y a dans _GET $count argument(s).";
+    
+    echo "<table>";
+        echo "<thead class='entete'>";
+            echo "<tr>
+                <th>Clé</th>
+                <th>Valeur</th>
+             </tr>";
+        echo "</thead>";
+        echo "<tbody>";
+            foreach($_GET as $key => $val){
+                if ($val) {
+                    echo "<tr>
+                        <th>$key</th>
+                        <th>$val</th>
+                    </tr>";
+                } else {
+                    echo "<tr>
+                        <th>$key</th>
+                        <th>Pas de valeur</th>
+                    </tr>";
+                }
+            }
+        echo "</tbody>";
+    echo "</table>";
 ?>
